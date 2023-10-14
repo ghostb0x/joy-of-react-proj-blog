f02894b18fcc7954bdb052aa8e7d6b2ddfe8a6b5
ghostb0x <ghostb0x.github@gmail.com>
2023-10-14 05:47:41 -0700

    Step 3 - Add metadata to main layout and dynamically generate for post pages. Add React.cache to database call function to reduce number of calls

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import { loadBlogPost } from '@/helpers/file-helpers';
 import BlogHero from '@/components/BlogHero';
+import { BLOG_TITLE } from '@/constants';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 import styles from './postSlug.module.css';
 
+export async function generateMetadata({ params }) {
+  const slug = params.postSlug;
+  const postData = await loadBlogPost(slug);
+  const title = postData.frontmatter.title;
+  const abstract = postData.frontmatter.abstract;
+  
+  return {
+    title: `${title} • ${BLOG_TITLE}`,
+    description: abstract,
+  }
+
+}
 async function BlogPost({ params }) {
   const slug = params.postSlug;
   const postData = await loadBlogPost(slug);
--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 import BlogSummaryCard from '@/components/BlogSummaryCard';
 import styles from './homepage.module.css';
-
 //uses node fs system to fetch post data from local directory
 import { getBlogPostList } from '@/helpers/file-helpers';
+
 
 async function Home() {
   const currentPosts = await getBlogPostList();
@@ -13,7 +13,6 @@ async function Home() {
     <div className={styles.wrapper}>
       <h1 className={styles.mainHeading}>Latest Content:</h1>
 
-      
       {currentPosts.map(({ slug, title, abstract, publishedOn }) => (
         <BlogSummaryCard
           key={slug}

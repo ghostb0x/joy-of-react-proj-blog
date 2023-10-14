127dc618e3852c8f2de85d79cdb1692df5fc095a
ghostb0x <ghostb0x.github@gmail.com>
2023-10-14 05:55:16 -0700

    Step 4 + 5a: add CodeSnippet and DivisionGroupsDemo components to MDXRemote components list for rendering on posts

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -3,6 +3,8 @@ import { loadBlogPost } from '@/helpers/file-helpers';
 import BlogHero from '@/components/BlogHero';
 import { BLOG_TITLE } from '@/constants';
 import { MDXRemote } from 'next-mdx-remote/rsc';
+import CodeSnippet from '@/components/CodeSnippet';
+import DivisionGroupsDemo from '@/components/DivisionGroupsDemo';
 import styles from './postSlug.module.css';
 
 export async function generateMetadata({ params }) {
@@ -29,7 +31,12 @@ async function BlogPost({ params }) {
         publishedOn={postData.frontmatter.publishedOn}
       />
       <div className={styles.page}>
-        <MDXRemote source={content} />
+        <MDXRemote source={content} 
+        components={{
+          pre: CodeSnippet,
+          DivisionGroupsDemo
+        }}
+        />
       </div>
     </article>
   );

80a4bc446c4b3a1746aa9e1f17dc0d80e8fe7079
ghostb0x <ghostb0x.github@gmail.com>
2023-10-14 04:14:06 -0700

    Step 1 - fetch blog posts and map over returned array to show snippets on home

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,28 @@
 import React from 'react';
 
 import BlogSummaryCard from '@/components/BlogSummaryCard';
-
 import styles from './homepage.module.css';
 
-function Home() {
+//uses node fs system to fetch post data from local directory
+import { getBlogPostList } from '@/helpers/file-helpers';
+
+async function Home() {
+  const currentPosts = await getBlogPostList();
+
   return (
     <div className={styles.wrapper}>
-      <h1 className={styles.mainHeading}>
-        Latest Content:
-      </h1>
+      <h1 className={styles.mainHeading}>Latest Content:</h1>
 
-      {/* TODO: Iterate over the data read from the file system! */}
-      <BlogSummaryCard
-        slug="example"
-        title="Hello world!"
-        abstract="This is a placeholder, an example which shows how the “BlogSummaryCard” component should be used. You'll want to swap this out based on the data from the various MDX files!"
-        publishedOn={new Date()}
-      />
+      
+      {currentPosts.map(({ slug, title, abstract, publishedOn }) => (
+        <BlogSummaryCard
+          key={slug}
+          slug={slug}
+          title={title}
+          abstract={abstract}
+          publishedOn={publishedOn}
+        />
+      ))}
     </div>
   );
 }

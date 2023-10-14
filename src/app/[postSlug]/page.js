b4e34746098e2b8d5b5058497abf8c52f2c3ce8c
ghostb0x <ghostb0x.github@gmail.com>
2023-10-14 06:16:48 -0700

    Step 5A2 - update DivisionGroupsDemo import on post to use next's dynamic lazy-loading

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -4,26 +4,32 @@ import BlogHero from '@/components/BlogHero';
 import { BLOG_TITLE } from '@/constants';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 import CodeSnippet from '@/components/CodeSnippet';
-import DivisionGroupsDemo from '@/components/DivisionGroupsDemo';
 import styles from './postSlug.module.css';
+import dynamic from 'next/dynamic';
+import Spinner from '@/components/Spinner';
+
+// import DivisionGroupsDemo from '@/components/DivisionGroupsDemo';
+const LazyDivisionGroupsDemo = dynamic(
+  () => import('@/components/DivisionGroupsDemo'),
+  { loading: Spinner}
+)
 
 export async function generateMetadata({ params }) {
   const slug = params.postSlug;
   const postData = await loadBlogPost(slug);
   const title = postData.frontmatter.title;
   const abstract = postData.frontmatter.abstract;
-  
+
   return {
     title: `${title} • ${BLOG_TITLE}`,
     description: abstract,
-  }
-
+  };
 }
 async function BlogPost({ params }) {
   const slug = params.postSlug;
   const postData = await loadBlogPost(slug);
-
   const content = postData.content;
+
   return (
     <article className={styles.wrapper}>
       <BlogHero
@@ -31,11 +37,12 @@ async function BlogPost({ params }) {
         publishedOn={postData.frontmatter.publishedOn}
       />
       <div className={styles.page}>
-        <MDXRemote source={content} 
-        components={{
-          pre: CodeSnippet,
-          DivisionGroupsDemo
-        }}
+        <MDXRemote
+          source={content}
+          components={{
+            pre: CodeSnippet,
+            DivisionGroupsDemo: LazyDivisionGroupsDemo,
+          }}
         />
       </div>
     </article>
